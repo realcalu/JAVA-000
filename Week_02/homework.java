e8ec431141610f5b6a000aaffa9979dd4a35ccfc
洪鸿辉 <honghonghui@58.com>
2020-10-28 23:43:00 +0800

    Create homework.java

---
+++ Week_02/homework.java
@@ -0,0 +1,69 @@
+package src.calss01;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @version 1.0
+ * @Description TODO
+ * @Author honghonghui
+ * @Date
+ **/
+public class homework {
+    public static void main(String[] args) {
+        String url = "http://localhost:8801";
+        get(url);
+    }
+    private static CloseableHttpClient httpClient;
+
+    static {
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+        cm.setMaxTotal(100);
+        cm.setDefaultMaxPerRoute(20);
+        cm.setDefaultMaxPerRoute(50);
+        httpClient = HttpClients.custom().setConnectionManager(cm).build();
+    }
+
+    public static String get(String url) {
+        CloseableHttpResponse response = null;
+        BufferedReader in = null;
+        String result = "";
+        try {
+            HttpGet httpGet = new HttpGet(url);
+            RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30000).setConnectionRequestTimeout(30000).setSocketTimeout(30000).build();
+            httpGet.setConfig(requestConfig);
+            httpGet.setConfig(requestConfig);
+            httpGet.addHeader("Content-type", "application/json; charset=utf-8");
+            httpGet.setHeader("Accept", "application/json");
+            response = httpClient.execute(httpGet);
+            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer sb = new StringBuffer("");
+            String line = "";
+            String NL = System.getProperty("line.separator");
+            while ((line = in.readLine()) != null) {
+                sb.append(line + NL);
+            }
+            in.close();
+            result = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != response) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
